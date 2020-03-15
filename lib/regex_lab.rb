b5bd97dbdbc99854784a91acd868a2e44a3cda19
Isaac Smith <isaacsmith215@aol.com>
2020-03-15 14:45:46 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,8 +7,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  array = []
   if text.scan(/un\w+ing\b/)
-    return true
+    array << text.scan(/un\w+ing\b/)
   else
     return false
   end
